6e9d3b5235e62297a15dc01ae6ff3dcd3b594aa3
kostadinrusalov <rusalov@uni-sofia.bg>
2023-06-12 19:35:44 +0300

    add paircomparison operators

--- Pair/Pair.hpp
+++ Pair/Pair.hpp
@@ -10,21 +10,24 @@ struct Pair {
     T first;
     U second;
 
-    Pair() = default;
+    Pair();
 
     Pair(const T &first, const U &second);
 
-    Pair(T &&first, U &&second);
+    Pair(T &&first, U &&second) noexcept;
 
     void swap(Pair<T, U> &with) noexcept;
 };
+
+template<class T, class U>
+Pair<T, U>::Pair() : first{}, second{} {}
 
 template<class T, class U>
 Pair<T, U>::Pair(const T &first, const U &second)
         : first{first}, second{second} {}
 
 template<class T, class U>
-Pair<T, U>::Pair(T &&first, U &&second)
+Pair<T, U>::Pair(T &&first, U &&second) noexcept
         : first{std::move(first)}, second{std::move(second)} {}
 
 template<class T, class U>
@@ -32,4 +35,34 @@ void Pair<T, U>::swap(Pair<T, U> &with) noexcept {
     Pair<T, U> temp(*this);
     *this = with;
     with = temp;
+}
+
+template<class T1, class U1, class T2, class U2>
+bool operator==(const Pair<T1, U1> &rhs, const Pair<T2, U2> &lhs) {
+    return rhs.first == lhs.first && rhs.second == rhs.second;
+}
+
+template<class T1, class U1, class T2, class U2>
+bool operator!=(const Pair<T1, U1> &rhs, const Pair<T2, U2> &lhs) {
+    return !(rhs == lhs);
+}
+
+template<class T1, class U1, class T2, class U2>
+bool operator<(const Pair<T1, U1> &rhs, const Pair<T2, U2> &lhs) {
+    return rhs.first < lhs.first && rhs.second < rhs.second;
+}
+
+template<class T1, class U1, class T2, class U2>
+bool operator<=(const Pair<T1, U1> &rhs, const Pair<T2, U2> &lhs) {
+    return rhs < lhs || rhs == lhs;
+}
+
+template<class T1, class U1, class T2, class U2>
+bool operator>(const Pair<T1, U1> &rhs, const Pair<T2, U2> &lhs) {
+    return !(rhs <= lhs);
+}
+
+template<class T1, class U1, class T2, class U2>
+bool operator>=(const Pair<T1, U1> &rhs, const Pair<T2, U2> &lhs) {
+    return !(rhs < lhs);
 }
