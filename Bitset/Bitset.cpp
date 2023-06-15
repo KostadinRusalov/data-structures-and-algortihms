aa1987b7ab994aa2d136993d65d5c19afc0a17d5
kostadinrusalov <rusalov@uni-sofia.bg>
2023-06-15 16:42:52 +0300

    add bitset

---
+++ Bitset/Bitset.cpp
@@ -0,0 +1,156 @@
+#include "Bitset.h"
+#include <utility>
+
+Bitset::Bitset(value_type max)
+        : max{max}, count{0} {
+    data = new Bucket[bucketCount()]{};
+}
+
+Bitset::Bitset(const Bitset &other) {
+    copyFrom(other);
+}
+
+Bitset::Bitset(Bitset &&other) noexcept {
+    moveFrom(std::move(other));
+}
+
+Bitset &Bitset::operator=(const Bitset &other) {
+    if (this != &other) {
+        free();
+        copyFrom(other);
+    }
+    return *this;
+}
+
+Bitset &Bitset::operator=(Bitset &&other) noexcept {
+    if (this != &other) {
+        free();
+        moveFrom(std::move(other));
+    }
+    return *this;
+}
+
+Bitset::~Bitset() {
+    free();
+}
+
+bool Bitset::empty() const {
+    for (size_type b = 0; b < bucketCount(); ++b) {
+        if (data[b] != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+Bitset::size_type Bitset::size() const {
+    return count;
+}
+
+Bitset::value_type Bitset::capacity() const {
+    return max;
+}
+
+void Bitset::clear() {
+    for (size_type b = 0; b < bucketCount(); ++b) {
+        data[b] = 0;
+    }
+}
+
+void Bitset::add(Bitset::value_type num) {
+    if (num >= max) {
+        resize(num);
+    }
+    size_type bucketIdx = bucket(num);
+    position_idx pos = position(num);
+
+    if (!(data[bucketIdx] & pos)) {
+        count++;
+    }
+    data[bucketIdx] |= pos;
+}
+
+template<class Collection>
+void Bitset::add(const Collection &collection) {
+    for (auto el: collection) {
+        add(el);
+    }
+}
+
+void Bitset::remove(Bitset::value_type num) {
+    if (num >= max) {
+        return;
+    }
+    size_type bucketIdx = bucket(num);
+    position_idx pos = position(num);
+
+    if (data[bucketIdx] & pos) {
+        count--;
+    }
+    data[bucketIdx] &= ~pos;
+}
+
+bool Bitset::contains(Bitset::value_type num) const {
+    if (num >= max) {
+        return false;
+    }
+    return data[bucket(num)] & position(num);
+}
+
+void Bitset::free() {
+    delete[] data;
+    data = nullptr;
+}
+
+void Bitset::copyFrom(const Bitset &other) {
+    max = other.max;
+    count = other.max;
+
+    size_type buckets = bucketCount();
+    data = new Bucket[buckets];
+    for (size_type b = 0; b < buckets; ++b) {
+        data[b] = other.data[b];
+    }
+}
+
+void Bitset::moveFrom(Bitset &&other) {
+    max = other.max;
+    count = other.max;
+
+    data = other.data;
+    other.data = nullptr;
+}
+
+void Bitset::resize(Bitset::value_type newMax) {
+    if (newMax < max) {
+        return;
+    }
+    size_type oldCount = bucketCount();
+
+    max = newMax;
+    auto temp = new Bucket[bucketCount()]{};
+    for (size_type b = 0; b < oldCount; ++b) {
+        temp[b] = data[b];
+    }
+
+    free();
+    data = temp;
+}
+
+Bitset::size_type Bitset::bucketCount() const {
+    return max / BUCKET_SIZE;
+}
+
+Bitset::size_type Bitset::bucket(Bitset::value_type num) const {
+    return num / BUCKET_SIZE;
+}
+
+Bitset::position_idx Bitset::position(Bitset::value_type num) const {
+    size_type shift = (BUCKET_SIZE - 1) - num % BUCKET_SIZE;
+    return 1 << shift;
+}
+
+
+
+
+
---
+++ Bitset/Bitset.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+class Bitset {
+public:
+    typedef uint64_t Bucket;
+    typedef size_t size_type;
+    typedef unsigned value_type;
+    typedef uint8_t position_idx;
+
+    friend class BitsetIterator;
+
+    static const unsigned short ONE_BYTE = 8;
+    static const size_type BUCKET_SIZE = ONE_BYTE * sizeof(Bucket);
+private:
+    Bucket *data;
+    value_type max;
+    size_type count;
+
+public:
+    explicit Bitset(value_type max);
+
+    Bitset(const Bitset &other);
+
+    Bitset(Bitset &&other) noexcept;
+
+    Bitset &operator=(const Bitset &other);
+
+    Bitset &operator=(Bitset &&other) noexcept;
+
+    ~Bitset();
+
+    // capacity
+    bool empty() const;
+
+    size_type size() const;
+
+    value_type capacity() const;
+
+    // modifiers
+    void clear();
+
+    void add(value_type num);
+
+    template<class Collection>
+    void add(const Collection &collection);
+
+    void remove(value_type num);
+
+    // element access
+    bool contains(value_type num) const;
+
+private:
+    void free();
+
+    void copyFrom(const Bitset &other);
+
+    void moveFrom(Bitset &&other);
+
+    void resize(value_type newMax);
+
+    size_type bucketCount() const;
+
+    size_type bucket(value_type num) const;
+
+    position_idx position(value_type num) const;
+};
